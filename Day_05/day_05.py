e1463a9c3818481a29a8f292de9972ba5e6b356a
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-02-26 08:09:46 +0200

    Dictionary

--- Day_05/day_05.py
+++ Day_05/day_05.py
@@ -12,17 +12,23 @@
 # print(unscrambled_str)
 
 
-import math
+books = [
+    {"title": "Infinite Jest", "rating": 4.5, "genre": "Fiction"},
+    {"title": "The Catcher in the Rye", "rating": 3.9, "genre": "Fiction"},
+    {"title": "Sapiens", "rating": 4.9, "genre": "History"},
+    {"title": "A Brief History of Time", "rating": 4.8, "genre": "Science"},
+    {"title": "Clean Code", "rating": 4.7, "genre": "Technology"},
+]
 
-points = [(3, 4), (6, 12), (10, 13)]
+title_list = []
+for title in books:
+    if title["rating"] >= 4.7:
+        title_list.append(title["title"])
 
-# Expected Output
-# distances = [5.0, 13.42, 16.4]
-
-distances = []
-
-for point in points:
-    a, b = point
-    d = math.sqrt(a**2 + b**2)
-    distances.append(round(d, 2))
-print(distances)
+if len(title_list) >= 2:
+    and_index = len(title_list) - 1
+    title_list.insert(and_index, "and")
+title_str = ", ".join(title_list)
+print(title_str)
+output = title_str.replace(", and,", " and")
+print(f"Highest rated books are: {output}")
