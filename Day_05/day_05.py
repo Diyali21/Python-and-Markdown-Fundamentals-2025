90589dcdf624382c7c384c7f2926daa844fa3d07
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-02-26 09:32:00 +0200

    solutions to dictionary

--- Day_05/day_05.py
+++ Day_05/day_05.py
@@ -15,20 +15,20 @@
 books = [
     {"title": "Infinite Jest", "rating": 4.5, "genre": "Fiction"},
     {"title": "The Catcher in the Rye", "rating": 3.9, "genre": "Fiction"},
-    {"title": "Sapiens", "rating": 4.9, "genre": "History"},
+    {"title": "Sapiens", "rating": 3.9, "genre": "History"},
     {"title": "A Brief History of Time", "rating": 4.8, "genre": "Science"},
     {"title": "Clean Code", "rating": 4.7, "genre": "Technology"},
 ]
 
-title_list = []
-for title in books:
-    if title["rating"] >= 4.7:
-        title_list.append(title["title"])
+# titles = []
+# for book in books:
+#     if book["rating"] >= 4.7:
+#         titles.append(book["title"])
 
-if len(title_list) >= 2:
-    and_index = len(title_list) - 1
-    title_list.insert(and_index, "and")
-title_str = ", ".join(title_list)
-print(title_str)
-output = title_str.replace(", and,", " and")
-print(f"Highest rated books are: {output}")
+# if len(titles) >= 2:
+#     and_index = len(titles) - 1
+#     titles.insert(and_index, "and")
+# title_str = ", ".join(titles)
+# print(title_str)
+# output = title_str.replace(", and,", " and")
+# print(f"Highest rated books are: {output}")
