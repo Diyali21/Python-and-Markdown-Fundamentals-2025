64360cafc129a7cd72e012daed20731909708e70
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-02-26 11:08:11 +0200

    safe access dictionary

--- Day_06/day_06.py
+++ Day_06/day_06.py
@@ -3,25 +3,5 @@ inventory = [
     {"name": "Banana 🍌", "quantity": 20, "price": 0.2},
 ]
 
-item_name = input("What is the product name? ")
-item_qty = int(input("What is the quantity? "))
-item_price = float(input("What is the price? "))
-
-product = {"name: ": item_name, "quantity: ": item_qty, "price: ": item_price}
-
-inventory.append(product)
-
-
-continue_input = input("Do you want to continue? ").lower()
-
-while continue_input == "yes":
-    item_name = input("What is the product name? ")
-    item_qty = int(input("What is the quantity? "))
-    item_price = float(input("What is the price? "))
-
-    product = {"name: ": item_name, "quantity: ": item_qty, "price: ": item_price}
-
-    inventory.append(product)
-    continue_input = input("Do you want to continue? ").lower()
-
-print(inventory)
+for i in range(len(inventory)):
+    print(inventory[i].get("name"))
