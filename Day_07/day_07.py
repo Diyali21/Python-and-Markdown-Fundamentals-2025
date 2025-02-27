5a92180f8879c0755a07b636fcecb0265917ad36
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-02-27 11:49:15 +0200

    list comp solutions updated

--- Day_07/day_07.py
+++ Day_07/day_07.py
@@ -1,17 +1,26 @@
-movies = [
-    {"title": "Inception", "ratings": [5, 4, 5, 4, 5]},
-    {"title": "Interstellar", "ratings": [5, 5, 4, 5, 4]},
-    {"title": "Dunkirk", "ratings": [4, 4, 4, 3, 4]},
-    {"title": "The Dark Knight", "ratings": [5, 5, 5, 5, 5]},
-    {"title": "Memento", "ratings": [4, 5, 4, 5, 4]},
-]
+# movies = [
+#     {"title": "Inception", "ratings": [5, 4, 5, 4, 5]},
+#     {"title": "Interstellar", "ratings": [5, 5, 4, 5, 4]},
+#     {"title": "Dunkirk", "ratings": [4, 4, 4, 3, 4]},
+#     {"title": "The Dark Knight", "ratings": [5, 5, 5, 5, 5]},
+#     {"title": "Memento", "ratings": [4, 5, 4, 5, 4]},
+# ]
 
-# [{**movies, *movie.get("average_rating", sum(movie["ratings"]) / len(movies))} for movie in movies]
+# # [{**movies, *movie.get("average_rating", sum(movie["ratings"]) / len(movies))} for movie in movies]
+
+# # print(
+# #     [{**movie, "avg_rating": sum(movie["ratings"]) / len(movies)} for movie in movies]
+# # )
 
 # print(
-#     [{**movie, "avg_rating": sum(movie["ratings"]) / len(movies)} for movie in movies]
+#     [movie["title"] for movie in movies if sum(movie["ratings"]) / len(movies) >= 4.6]
 # )
 
-print(
-    [movie["title"] for movie in movies if sum(movie["ratings"]) / len(movies) >= 4.6]
-)
+# Task
+# Creating the below dictionary with for loop
+
+# {"1": 1, "2": 4, "3": 9, "4": 16, "5": 25}
+
+for i in range(1, 6):
+    square = {f"{i}": i * i}
+    print(square)
