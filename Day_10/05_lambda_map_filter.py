c2ed3b4e63128217ab398a6cd6587ff7ff054b30
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-03-04 13:18:03 +0200

    lambda map filter

---
+++ Day_10/05_lambda_map_filter.py
@@ -0,0 +1,72 @@
+def greet(name):
+    return f"Hi, {name}"
+
+
+# Lambda expression/ Lambda Function: Function one liner
+
+greet1 = lambda name: f"Hi, {name}"
+
+print(greet("Jevan"))
+print(greet1("Jevan"))
+
+
+# Lambda expression
+# 1. Anonymous - nameless function
+# 2. One liner
+# 3. Implicit return (automatically)
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+add1 = lambda n1, n2: n1 + n2
+
+print(add1(9, 8))
+
+
+# map & filter
+
+
+player_stats = [10, 30, 60]
+boosted_stats = map(lambda x: x * 2, player_stats)
+print(boosted_stats, list(boosted_stats))
+
+# map -> fn -> Takes another fn as arg
+# map -> Higher order functions
+boosted_stats = map(lambda x: x * 2, [10, 30, 60])
+
+dbl = lambda x: x * 2
+boosted_stats = map(dbl, [10, 30, 60])
+
+print(boosted_stats, list(boosted_stats))
+
+
+def dbl1(x):
+    return x * 2
+
+
+boosted_stats = map(dbl1, [10, 30, 60])
+print(boosted_stats, list(boosted_stats))
+
+
+boosted_stats = map(lambda x: x * 2, player_stats)
+print(boosted_stats, list(boosted_stats))
+
+# 1. def - Reuse else where
+# 2. lambda - one time use, concise
+
+
+# filter -> HOF function
+# HOF -> map, filter
+# result = filter(lambda x: x > 10, [10, 30, 60])
+# print(result, list(result))
+
+gt1 = lambda x: x > 10  # Predicate: Returns boolean
+result = filter(gt1, [10, 30, 60])
+print(result, list(result))
+
+# gt1(10) -> F
+# gt1(30) -> T
+# gt1(60) -> T
+# [30, 60]
