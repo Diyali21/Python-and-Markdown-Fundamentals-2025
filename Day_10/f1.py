aec2447e58ea21ffc08d02ad362b1aa16f5173de
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-03-04 09:10:41 +0200

    main functions

---
+++ Day_10/f1.py
@@ -0,0 +1,11 @@
+def add(t1, t2):
+    return t1 + t2
+
+
+def main():
+    print("Hello, from f1")
+    print(__name__)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Day_10/f2.py
@@ -0,0 +1,10 @@
+import f1  # Imports cause Entire file to run
+
+
+def main():
+    print("Hello, from f2")
+    print("Sum:", f1.add(10, 100))
+
+
+if __name__ == "__main__":
+    main()
