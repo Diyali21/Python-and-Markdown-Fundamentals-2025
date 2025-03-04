35a14fbb6b824f89e39b2a35e82100c00e984492
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-03-04 08:08:34 +0200

    assignment- formatting output

---
+++ Day_09/assignment.py
@@ -0,0 +1,45 @@
+from ctypes import sizeof
+from datetime import datetime
+
+items = [
+    {"name": "Widget A", "quantity": 10, "price": 2.5},
+    {"name": "Widget B", "quantity": 5, "price": 15.75},
+    {"name": "Gadget", "quantity": 2, "price": 99.99},
+]
+
+# Invoice Date: 03 March 2025
+
+# Product               Qty     Unit Price          Total
+# -------------------------------------------------------
+# Widget A               10           2.50          25.00
+# Widget B                5          15.75          78.75
+# Gadget                  2          99.99         199.98
+# -------------------------------------------------------
+# Grand Total                                      303.73
+
+
+def print_invoice(invoice_date, items):
+    print(f"Invoice Date: {invoice_date} \n\n")
+
+    print(f"{'Product':<21} {'Qty':<7} {'Unit Price':<19} {'Total'}")
+
+    print(f"{'-':-<55}")
+    grand_total = 0
+    for item in items:
+        name = item["name"]
+
+        qty = item["quantity"]
+
+        up = item["price"]
+
+        total = qty * up
+
+        grand_total = grand_total + total
+
+        print(f"{name:<22} {qty:<11} {up:<14} {total:.2f}")
+
+    print(f"{'-':-<55}")
+    print(f"{'Grand Total':<50}{grand_total}")
+
+
+print_invoice(datetime.now(), items)
---
+++ Day_09/cool.py
@@ -0,0 +1,8 @@
+def to_upper_case(text):
+    return text.upper() + " 👋"
+
+
+# Only when cool.py execute -> Jamie
+if __name__ == "__main__":
+    print(__name__)  # dunder variables
+    print(to_upper_case("Jamie"))
